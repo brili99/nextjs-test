e0a31b3d49eeed2ed4a7112f6c56f15d2f6e21ad
Muhammad Brilian Erranaomi <brilianeranaomi@gmail.com>
2021-05-11 11:44:58 +0700

    update api device

--- pages/api/device_only.js
+++ pages/api/device_only.js
@@ -1,6 +1,8 @@
+import Cors from 'cors'
 const admin = require('firebase-admin');
 const serviceAccount = require('../../nextjs-test-bf155-firebase-adminsdk-ndxyk-c62351b010.json');
 var url = require('url');
+
 
 if (!admin.apps.length) {
     admin.initializeApp({
@@ -9,12 +11,62 @@ if (!admin.apps.length) {
 }
 const db = admin.firestore();
 
-export default (req, res) => {
+// export default (req, res) => {
+//     var device = req.query.device;
+//     var timestamp = admin.firestore.Timestamp.now();
+//     var hour_now = timestamp.toDate().getHours();
+//     var minute_now = timestamp.toDate().getMinutes();
+//     db.collection('device').doc(device).update({
+//         'last_online': timestamp
+//     });
+
+//     db.collection('device').doc(device).get().then(queryResult => {
+//         const arr_jadwal = queryResult.data().jadwal;
+//         const arr_time = [];
+//         arr_jadwal.forEach(jadwal => {
+//             var time = "";
+//             time += jadwal.toDate().getHours() + ":";
+//             time += jadwal.toDate().getMinutes() + ":";
+//             time += jadwal.toDate().getSeconds();
+//             arr_time.push(time);
+//         });
+
+//         res.statusCode = 200;
+//         res.json({
+//             device: device,
+//             timestamp: timestamp.toMillis(),
+//             jadwal: arr_jadwal
+//         });
+//     });
+// }
+
+async function handler(req, res) {
     var device = req.query.device;
-    db.collection('device').doc(device).set({
-        'last_online': admin.firestore.Timestamp.now()
+    var timestamp = admin.firestore.Timestamp.now();
+    var hour_now = timestamp.toDate().getHours();
+    var minute_now = timestamp.toDate().getMinutes();
+    await db.collection('device').doc(device).update({
+        'last_online': timestamp
     });
 
-    res.statusCode = 200
-    res.json({ device: device })
+    await db.collection('device').doc(device).get().then(queryResult => {
+        const arr_jadwal = queryResult.data().jadwal;
+        const arr_time = [];
+        arr_jadwal.forEach(jadwal => {
+            var time = "";
+            time += jadwal.toDate().getHours() + ":";
+            time += jadwal.toDate().getMinutes() + ":";
+            time += jadwal.toDate().getSeconds();
+            arr_time.push(time);
+        });
+
+        res.statusCode = 200;
+        res.json({
+            device: device,
+            timestamp: timestamp.toMillis(),
+            jadwal: arr_jadwal
+        });
+    });
 }
+
+export default handler
